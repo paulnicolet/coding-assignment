bfa0c4bb77f573a1835a7ec16532882fbdb7fd4f
Paul Nicolet <nicoletpaul@icloud.com>
2020-09-30 15:05:49 +0200

    Add command line args parsing

--- run.py
+++ run.py
@@ -1,25 +1,46 @@
+import argparse
 from pathlib import Path
 
-from src.loader import ScenarioLoader
 from src.database import Database
 from src.eval import accuracy
+from src.loader import ScenarioLoader
 
+DEFAULT_SCENARIO = 3
 SCENARIOS_PATH = Path('.') / 'data' / 'scenarios'
 
 
 def run(scenario_id):
+    # Load scenario
     loader = ScenarioLoader(SCENARIOS_PATH, scenario_id)
 
+    # Execute database operations
     db = Database(loader.graph_root)
     db.add_nodes(loader.graph_tail)
     db.add_extract(loader.extract)
     db.add_nodes(loader.graph_edits)
 
+    # Get result
     status = db.get_extract_status()
-
     acc = accuracy(loader.expected_status, status)
-    print(acc)
+    print(f'Accuracy for scenario {scenario_id}: {acc}')
+
+
+def _parse_scenario_id():
+    parser = argparse.ArgumentParser(
+        description='Foodvisor dynamic class structure'
+    )
+
+    parser.add_argument(
+        '-sid',
+        '--scenario_id',
+        type=int,
+        required=False,
+        default=DEFAULT_SCENARIO,
+        help=f'The scenario id to execute. Resources must be available under {SCENARIOS_PATH}'
+    )
+
+    return parser.parse_args().scenario_id
 
 
 if __name__ == "__main__":
-    run(3)
+    run(_parse_scenario_id())
